f8d9018933357585e6a02f07e1ce1bdcd91e099d
Lionel <lpei1900@gmail.com>
2024-06-04 09:59:14 -0400

    -Created popup after clicking

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ const timerDiv = document.querySelector(".timer");
 const cpsDiv = document.querySelector(".cps");
 const clicksDiv = document.querySelector(".clicks");
 
-const btnTen = document.querySelector(".btnTen");
-const btnFive = document.querySelector(".btnFive");
+/*const fiveBtn = document.querySelector("#fiveBtn");
+const tenBtn = document.querySelector("#tenBtn");
+fiveBtn.addEventListener("click", () => gameTime = 5);
+tenBtn.addEventListener("click", () => gameTime = 10);
+*/
+
+
 
 let gameOn = false;
 let counter = 0;
 let cps = 0;
 let clicks = 0;
 let startTime = 0;
-let gameTime = 5;
+let gameTime = 3;
 
 
 clickArea.addEventListener("click", () => startGame(gameTime));
-btnTen.addEventListener("click", () => gameTime = 10);
-btnFive.addEventListener("click", () => gameTime = 5);
+
 
 function startGame(seconds){
     if(!gameOn){
@@ -25,7 +29,8 @@ function startGame(seconds){
         updateClicks();
         let timer = setInterval(updateClock, 10);
         setTimeout(() => resetGame(timer), seconds * 1000);
-    }else{
+    }
+    else{
         updateClicks();
     }
 }
@@ -46,6 +51,7 @@ function updateClock(){
     timerDiv.textContent = `${seconds}.${milliseconds}`;
 
 }
+
 function updateClicks(){
     clicks+=1;
     clicksDiv.textContent = clicks;
@@ -53,10 +59,31 @@ function updateClicks(){
 
 function resetGame(timer){
     clearInterval(timer);
+    showPopup();
     gameOn = false;
     counter = 0;
     cps = 0;
     clicks = 0;
     startTime = 0;
-
+    timerDiv.textContent = 0.0;
+    cpsDiv.textContent = 0;
+    clicksDiv.textContent = 0;
 }
+
+
+/*Popup*/
+document.getElementById("closeButton").addEventListener("click", hidePopup);
+document.getElementById("closePopup").addEventListener("click", hidePopup);
+
+function showPopup() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("clicksText").textContent = `Clicks: ${clicks}`;
+    document.getElementById("cpsText").textContent = `Cps: ${cps}`;
+}
+
+function hidePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+
+
